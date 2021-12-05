22cdbdb106d21de8ec6212b35215a9abdd91d344
Brian <bgswilde@gmail.com>
2021-12-04 18:20:25 -0600

    node api route to return data from whois api complete

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Image } from "react-bootstrap";
+
+function Header() {
+  return (
+    <div className="header">
+      <Image></Image>
+    </div>
+  )
+}
+
+export default Headers;
---
+++ server/routes/whois.js
@@ -0,0 +1,5 @@
+const router = require(express).Router();
+
+// refactor to move the route on the server.js file here to clean up code
+
+module.exports = router;
